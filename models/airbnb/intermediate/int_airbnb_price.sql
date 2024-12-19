f961a2503bea99462201f53f4a55ab444f68f4c1
Chen Ming Coudamy <chenbming@gmail.com>
2024-12-19 13:22:17 +0000

    build int_airbnb_price model as a table

---
+++ models/airbnb/intermediate/int_airbnb_price.sql
@@ -0,0 +1,11 @@
+{{config(materialized='table')}}
+
+SELECT
+    *
+    , CASE
+        WHEN price >= 1500 THEN 'luxury'
+        WHEN price <= 50 THEN 'cheap'
+        WHEN price BETWEEN 500 AND 1500 THEN 'expensive'
+        ELSE 'afordable'
+    END AS price_range
+FROM {{ref('stg_airbnb')}}
