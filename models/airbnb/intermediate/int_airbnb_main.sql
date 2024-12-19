aba28d15b11cfc816c25c15025cd12a63980a858
Chen Ming Coudamy <chenbming@gmail.com>
2024-12-19 18:02:57 +0000

    Modification on int_airbnb_main and price tables

--- models/airbnb/intermediate/int_airbnb_main.sql
+++ models/airbnb/intermediate/int_airbnb_main.sql
@@ -12,7 +12,8 @@ SELECT
         WHEN review_scores_value <= 4.0 THEN 'low'
         ELSE 'medium'
     END AS review_range,
-    ROUND(COUNTIF(available = true)/COUNT(DISTINCT (date)),2) AS availability_rate
+    ROUND(COUNTIF(available = true)/COUNT(DISTINCT (date)),2) AS availability_rate,
+    ROUND(1-COUNTIF(available = true)/COUNT(DISTINCT (date)),2) AS booking_rate
 FROM {{ref('stg_airbnb')}}
 GROUP BY listing_id, room_type, host_response_time, review_scores_value
 ORDER BY COUNTIF(available = true) DESC
